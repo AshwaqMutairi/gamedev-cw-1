72ca789d46d877412c72cbf28dcbc0813fb65c92
AshwaqMutairi <64764655+AshwaqMutairi@users.noreply.github.com>
2020-12-20 20:17:08 +0300

    cw1 v1

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace cw1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "mario";
+            int speed = 10;
+            int age = 20;
+            string sp1 = "FIRE";
+            string sp2 = "ICE";
+
+            Console.WriteLine("I'm {0} i'm {1} years old, my speed is {2}, I have {3} and {4} super powers.", name,age,speed,sp1,sp2);
+        }
+    }
+}
